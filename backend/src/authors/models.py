917b430028c570e702663caa7f775c6b651209b4
shobhitm23 <smakhij@purdue.edu>
2019-02-15 03:02:11 -0500

    Authors Backend - Have to add API endpoints and datafields

---
+++ backend/src/authors/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+
+    first_name = models.CharField(max_length=20, default="FirstName")
+    last_name = models.CharField(max_length=20, default="LastName")
+    review_count = models.IntegerField("Number of Reviews", default=0)
+    bio = models.TextField("About me", max_length=500, default=str(first_name+last_name))
+    #previousworks =
+
+
+    GENRES = (
+        ('FA', 'Fantasy'),
+        ('RO', 'Romance'),
+        ('TR', 'Thriller'),
+        ('MY', 'Mystery'),
+        ('BI', 'Biography'),
+        ('FI', 'Fiction'),
+        ('NF', 'Non Fiction'),
+        ('SF', 'Science Fiction'),
+    )
+
+    genre = models.CharField(max_length=2, choices = GENRES, default='FA')
