d24373fdf72d7667fc4c6adffceb861eaeb4c59e
shobhitm23 <smakhij@purdue.edu>
2019-02-16 10:39:43 -0500

    Removed List and inserted Detailed View

--- backend/src/authors/api/views.py
+++ backend/src/authors/api/views.py
@@ -3,10 +3,14 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 from authors.models import Author
 from .serializers import AuthorSerializer
 
-class AuthorWorktView(RetrieveAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
+#class AuthorWorktView(RetrieveAPIView):
+#    queryset = Author.objects.all()
+#    serializer_class = AuthorSerializer
 
-class AuthorListView(ListAPIView):
+#class AuthorListView(ListAPIView):
+#    queryset = Author.objects.all()
+#    serializer_class = AuthorSerializer
+
+class AuthorDetailView(RetrieveAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
