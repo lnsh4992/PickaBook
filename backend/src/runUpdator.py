5978c455aa932554fa4143a0ec7a6bb8f6b9a0e2
mKim1598 <kim2167@purdue.edu>
2019-04-07 19:35:43 -0400

    minor change on auto update

--- backend/src/runUpdator.py
+++ backend/src/runUpdator.py
@@ -6,11 +6,10 @@ def run():
 
     print("runUpdator called!")
     
-    started_day = datetime.datetime.today().weekday()
-    
     while(1):
         
-        if((started_day % 7) == 0):
+        today = datetime.datetime.today().weekday()
+        if((today % 7) == 0):
             book_authorCrawler.run()
         
         time.sleep(3600)
